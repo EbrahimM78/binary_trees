41406745ec1364cd6aff505287cf6ecf253a63bb
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-08-31 22:26:48 +0000

    Tasks 0 - 18, with tests and corrections

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,11 +9,16 @@
  *         Otherwise - a pointer to the sibling.
  */
 
-/* binary_tree_t *binary_tree_sibling(binary_tree_t *node) */
-/* { */
-if (node == NULL || node->parent == NULL)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+if (node)
+{
+if (!node->parent)
 return (NULL);
 if (node->parent->left == node)
 return (node->parent->right);
+if (node->parent->right == node)
 return (node->parent->left);
+}
+return (NULL);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -24,7 +24,7 @@ int n;
 struct binary_tree_s *parent;
 struct binary_tree_s *left;
 struct binary_tree_s *right;
-} binary_tree_t;
+};
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
